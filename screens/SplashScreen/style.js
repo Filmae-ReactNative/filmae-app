25a65641ef05097c341e7a3f0d0095c2ff0501e4
thaissan <thais.cavadas@hotmail.com>
2024-11-22 00:13:19 -0300

    adicionando tela de splash screen

---
+++ screens/SplashScreen/style.js
@@ -0,0 +1,37 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#14213D',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logoContainer: {
+    alignItems: 'center',
+  },
+  logo: {
+    width: 500,
+    height: 500,
+  },
+  textoContainer: {
+    alignItems: 'center',
+  },
+  bemVindo: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+    marginBottom: 0,
+  },
+  subtitulo: {
+    fontSize: 18,
+    color: '#E5E5E5',
+    textAlign: 'center',
+    paddingHorizontal: 20,
+  },
+  estrelas: {
+        width: 120,
+        height: 120,
+      },
+});
+
